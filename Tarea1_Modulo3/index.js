cab8972487d880a88b73195c9284adf327686ef4
Franco Velasco <francovelasco10@hotmail.com>
2021-10-21 12:23:46 -0500

    Entrega parcial Tarea 1 MODULO3

---
+++ Tarea1_Modulo3/index.js
@@ -0,0 +1,141 @@
+
+let cantidad = document.getElementById("cantidad");
+
+let de = document.getElementById("de");
+let a = document.getElementById("a");
+
+let dolar_col = 3783.10
+let dolar_mex = 20.25
+let dolar_euro = 0.86
+let dolar_libra = 0.72
+
+let mex_dolar = 0.049
+let mex_col = 186.81
+let mex_euro = 0.042
+let mex_libra = 0.036
+
+let col_dolar = 0.00027
+let col_mex = 0.0054
+let col_euro = 0.00023
+let col_libra = 0.00019
+
+let euro_dolar = 1.16
+let euro_mex = 23.57
+let euro_col = 4400.60
+let euro_libra= 0.84
+
+let libra_dolar = 1.38
+let libra_mex = 27.94
+let libra_col = 5219.40
+let libra_euro = 1.19
+
+let resultado = 0
+
+ 
+    let form = document.querySelector("form");
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+
+//DOLAR:
+
+if (de === 1 && a ===2) 
+{
+    resultado = cantidad * dolar_mex;
+} 
+else if(de === 1 && a === 3) 
+{
+    resultado == cantidad * dolar_col;
+} 
+else if(de === 1 && a === 4) 
+{
+    resultado == cantidad * dolar_euro;
+} 
+else if(de === 1 && a === 5) 
+{
+    resultado == cantidad * dolar_libra;
+} 
+
+//PESO MEX:
+
+if (de === 2 && a ===1) 
+{
+    resultado = cantidad * mex_dolar;
+} 
+else if(de === 2 && a === 3) 
+{
+    resultado == cantidad * mex_col;
+} 
+else if(de === 2 && a === 4) 
+{
+    resultado == cantidad * mex_euro;
+} 
+else if(de === 2 && a === 5) 
+{
+    resultado == cantidad * mex_libra;
+} 
+
+//PESO COL:
+
+if (de === 3 && a ===1) 
+{
+    resultado = cantidad * col_dolar;
+} 
+else if(de === 3 && a === 2) 
+{
+    resultado == cantidad * col_mex;
+} 
+else if(de === 3 && a === 4) 
+{
+    resultado == cantidad * col_euro;
+} 
+else if(de === 3 && a === 5) 
+{
+    resultado == cantidad * col_libra;
+} 
+
+//EURO:
+
+if (de === 4 && a ===1) 
+{
+    resultado = cantidad * euro_dolar;
+} 
+else if(de === 4 && a === 2) 
+{
+    resultado == cantidad * euro_mex;
+} 
+else if(de === 4 && a === 3) 
+{
+    resultado == cantidad * euro_col;
+} 
+else if(de === 4 && a === 5) 
+{
+    resultado == cantidad * euro_libra;
+} 
+
+//LIBRA ESTERLINA:
+
+if (de === 5 && a ===1) 
+{
+    resultado = cantidad * libra_dolar;
+} 
+else if(de === 5 && a === 2) 
+{
+    resultado == cantidad * libra_mex;
+} 
+else if(de === 5 && a === 3) 
+{
+    resultado == cantidad * libra_col;
+} 
+else if(de === 5 && a === 4) 
+{
+    resultado == cantidad * libra_euro;
+} 
+
+else{
+    resultado = cantidad;
+}
+
+
+document.getElementById("Resultado").innerHTML=""+resultado.toFixed(2);
+
+    })
